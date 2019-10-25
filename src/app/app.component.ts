4e5ed36b337ef67433213f294d01c354e7523295
codefactor-io <support@codefactor.io>
2019-10-25 11:35:11 +0000

    [CodeFactor] Apply fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,14 @@ import { samesizeConfig } from './interfaces/samesize.interface';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnDestroy{
-  
+export class AppComponent implements OnDestroy {
+
   imagesub: Subscription;
   images: Array<image> = [];
   samesizeConfig: samesizeConfig;
   displayconfig: DisplayConfig;
 
-  constructor(private imageservice: ImageService){
+  constructor(private imageservice: ImageService) {
     this.imagesub = this.imageservice.getImages().subscribe((images) => {
       this.images = images;
     });
@@ -33,15 +33,15 @@ export class AppComponent implements OnDestroy{
     this.samesizeConfig = {
       active: true,
       imgContainerHeight: '300px'
-    }
+    };
   }
 
-  logImage(img){
+  logImage(img) {
     console.log(img);
   }
 
   ngOnDestroy(): void {
     this.imagesub.unsubscribe();
   }
-  
+
 }
